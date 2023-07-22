db128d90e30807b4d211034f5199ad18f0d944ce
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-22 09:10:31 +0200

    render image when at current line and run command

--- lua/nvim-md-render-image/init.lua
+++ lua/nvim-md-render-image/init.lua
@@ -1,1 +1,36 @@
 print("Hello from nvim-md-render-image")
+
+-- workaround to satify linter who doesn't know
+-- about the vim global
+vim = vim
+
+local function render_image()
+  local line = tostring(vim.api.nvim_get_current_line())
+  local inline_link = line:match('!%[.-%]%(.-%)')
+
+  if not inline_link then
+    return
+  end
+
+  local uri = inline_link:match('%((.+)%)')
+  -- is this vulnerable to command injection?
+  local to_sixel_command = string.format("img2sixel -- \"%s\"", uri)
+  local handle = io.popen(to_sixel_command)
+
+  if not handle then
+    return
+  end
+
+  local data = handle:read("*a")
+  handle:close()
+  local stdout = vim.loop.new_tty(1, false)
+  stdout:write(data)
+end
+
+local function setup()
+  vim.api.nvim_create_user_command("RenderImage", render_image, { nargs = 0, desc = "Render image" })
+end
+
+return {
+  setup = setup
+}
