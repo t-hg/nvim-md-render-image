f288cca48342de7756be60616f54767cb0fb0eee
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-25 07:23:12 +0200

    schedule_wrap

--- lua/nvim-md-render-image/init.lua
+++ lua/nvim-md-render-image/init.lua
@@ -44,9 +44,11 @@ local function render_image()
   --  }
   --})
 
-  local stdout = vim.uv.new_tty(1, false)
+  local stdout = vim.loop.new_tty(1, false)
   --stdout:write("\x1b[2;0H")
-  stdout:write(data)
+  vim.schedule_wrap(function()
+    stdout:write(data)
+  end)
 end
 
 local function setup()
